dc94e72a2cdc154968ec6e3022f7ec1994b1e58d
Roman G. Serov <roman@serov.co>
2016-10-03 21:10:27 +0300

    add license header

--- ds1620.c
+++ ds1620.c
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2016 by Roman Serov <roman@serov.in>
+ * 
+ * This file is part of DS1620 demo.
+
+ * DS1620 demo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * DS1620 demo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with DS1620 demo. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+*/
+
 #include "ds1620.h"
 
 void ds_gpio_setup(void) {
